a2bd52329492d29938ea1967b38220cbfb88aa3d
jdungan <johnadungan@gmail.com>
2013-10-29 18:01:28 -0500

    improve query for hashtags and @

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ jQuery(document).ready(function () {
 	$('button').on('click',function(){
 		
 		//get the new search text
-		var find_text = $('input').val();
+		var find_query = encodeURIComponent($('input').val());
 		
 		//clear the list
 		$('ul').html("");
 		
 		//search twitter
-		$.getJSON("http://hack-lab.herokuapp.com/find?text='"+find_text+"'",function(data){
+		$.getJSON("http://hack-lab.herokuapp.com/find?text="+find_query,function(data){
 			// success!! 
 			$.each(data.statuses,function(index,value){
 				//add the tweet to the ul
